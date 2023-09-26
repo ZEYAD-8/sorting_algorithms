d783183945f328084f5991f9decf0680adb2aadd
MarwaAshraf1812 <marashraf090@gmail.com>
2023-09-26 03:55:22 +0300

    Header File Updated

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, unsigned int size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void swap_ints(int *a, int *b)
 void swap(int *x, int *y);
 void node_swap(listint_t *nx, listint_t *ny);
 
