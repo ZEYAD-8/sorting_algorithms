60e84fb0e80d1bcacb35a27c806e0daa949a55b9
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-26 04:05:20 +0300

    Header File - Updated

--- sort.h
+++ sort.h
@@ -21,11 +21,18 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-void bubble_sort(int *array, unsigned int size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void swap_ints(int *a, int *b);
+
 void swap(int *x, int *y);
+void bubble_sort(int *array, unsigned int size);
+
 void node_swap(listint_t *nx, listint_t *ny);
+void insertion_sort_list(listint_t **list);
+
+void swap_ints(int *a, int *b);
+void selection_sort(int *array, size_t size);
+
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 #endif
